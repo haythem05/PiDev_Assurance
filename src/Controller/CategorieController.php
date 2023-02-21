273d6b188d882bcc11500136fdccb072fc95a967
haythem <haythem.loueti@esprit.tn>
2023-02-21 04:13:11 +0100

    categorie image done+ modification

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormError;
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -83,26 +84,61 @@ public function addCategorie(ManagerRegistry $doctrine, Request $request)
     }
 
     return $this->renderForm('categorie/addCategorie.html.twig', [
-        'form' => $form,
+        'form' => $form->createView(),
     ]);
 }
 
 
 #[Route('/updateCategorie/{id}', name: 'updateCategorie')]
-               public function updateCategorie(CategorieRepository $repository,
-               $id,ManagerRegistry $doctrine,Request $request)
-               { //récupérer la categorie à modifier
-                   $categorie= $repository->find($id);
-                   $form=$this->createForm(CategorieFormType::class,$categorie);
-                   $form->handleRequest($request);
-                   if ($form->isSubmitted()&& $form->isValid() ) {
-                     
-                       $em =$doctrine->getManager();
-                       $em->flush();
-                       return $this->redirectToRoute("afficheCategorie"); }
-                      return $this->renderForm("categorie/addCategorie.html.twig", array("f" => $form));
+public function updateCategorie(CategorieRepository $repository, $id, Request $request, EntityManagerInterface $entityManager)
+{
+    $categorie = $repository->find($id);
+    $originalImage = $categorie->getCategorieImage(); // store the original image filename
 
+    // create the form and handle the request
+    $form = $this->createForm(CategorieFormType::class, $categorie);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $imageFile = $form->get('categorieImage')->getData(); // get the uploaded file
+
+        if ($imageFile) {
+            // generate a unique filename
+            $newFilename = uniqid().'.'.$imageFile->guessExtension();
+
+            // move the file to the images directory
+            $imageFile->move(
+                $this->getParameter('categorie_images_directory'),
+                $newFilename
+            );
+
+            // update the entity with the new filename
+            $categorie->setCategorieImage($newFilename);
+
+            // delete the original image file, if it exists
+            if ($originalImage) {
+                $originalImagePath = $this->getParameter('categorie_images_directory').'/'.$originalImage;
+                if (file_exists($originalImagePath)) {
+                    unlink($originalImagePath);
+                }
+            }
+        } else {
+            // use the original image filename
+            $categorie->setCategorieImage($originalImage);
+        }
+
+        // save the changes to the database
+        $entityManager->flush();
+
+        return $this->redirectToRoute('afficheCategorie');
+    }
+
+    return $this->render('categorie/addCategorie.html.twig', [
+        'form' => $form->createView(),
+    ]);
 }
+
+
 }
 
 
